249164facd51eaf8696cf4df6eaa6552b03fd747
Jay Carroll <jcarroll.it@gmail.com>
2011-11-06 23:26:51 -0500

    alter class names and add web views to storyboard

---
+++ Swyp/500pxViewController_iphone.h
@@ -0,0 +1,13 @@
+//
+//  500pxViewController.h
+//  Swyp
+//
+//  Created by Jay Carroll on 11/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface _500pxViewController_iphone : UIViewController
+
+@end
---
+++ Swyp/FlickrViewController_iphone.h
@@ -0,0 +1,13 @@
+//
+//  FlickrViewController.h
+//  Swyp
+//
+//  Created by Jay Carroll on 11/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrViewController_iphone : UIViewController
+
+@end
